21719f166ce146c4d1d591b5281afaabbc00c7c2
balu676 <balarajuannaldevu010@gmail.com>
2026-07-06 18:28:12 +0530

    added script for roboshop

---
+++ roboshop.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SG_ID="sg-0bcb724b5a131ee8b"
+AMI_ID="ami-07ac95f1ea03dd2e2"
+
+for instance in $@
+do
+    INSTANCE_ID=$( aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type "t3.micro" \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text )
+     if [ $instance == "frontend" ]; then
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+        )
+        
+    else
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )
+        
+    fi
+
+done
