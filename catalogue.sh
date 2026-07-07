b8007d5373bf9259d4204a455a4488be1412abf0
balu676 <balarajuannaldevu010@gmail.com>
2026-07-07 11:11:56 +0530

    added databse data step

--- catalogue.sh
+++ catalogue.sh
@@ -69,3 +69,18 @@ systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue 
 VALIDATE $? "Starting and enabling catalogue service"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y &>>$LOGS_FILE
+
+INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js
+    VALIDATE $? "Loading products"
+else
+    echo -e "Products already loaded ... $Y SKIPPING $N"
+fi
+
+systemctl restart catalogue
+VALIDATE $? "Restarting catalogue service"
