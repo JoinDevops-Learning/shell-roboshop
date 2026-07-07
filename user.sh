23920bd0be818ae4ff4dfb13c62b9829fc543191
balu676 <balarajuannaldevu010@gmail.com>
2026-07-07 22:49:51 +0530

    added user.sh

---
+++ user.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf module disable nodejs -y &>> $LOGS_FILE
+VALIDATE $? "Disabling nodejs module"
+
+dnf module enable nodejs:20 -y &>> $LOGS_FILE
+VALIDATE $? "Enabling nodejs module"
+
+dnf install nodejs -y &>> $LOGS_FILE
+VALIDATE $? "Installing nodejs"     
+
+if [ $? -ne 0 ]; then
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOGS_FILE
+VALIDATE $? "Creating roboshop system user"
+else
+    echo -e "$Y roboshop system user already exists $N" | tee -a $LOGS_FILE
+fi
+
+mkdir -p /app
+VALIDATE $? "Creating /app directory"
+
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip 
+VALIDATE $? "downloading user code"
+
+cd /app 
+VALIDATE $? "Changing directory to /app"
+
+rm -rf /app/*
+VALIDATE $? "Cleaning /app directory"
+
+unzip /tmp/user.zip &>> $LOGS_FILE
+VALIDATE $? "Extracting user code"
+
+npm install &>> $LOGS_FILE
+VALIDATE $? "Installing npm dependencies"
+
+cp SCRIPT_DIR/user.service /etc/systemd/system/user.service &>> $LOGS_FILE
+VALIDATE $? "created systemctl service"
+
+systemctl daemon-reload &>> $LOGS_FILE
+validate $? "Reloading systemctl daemon"
+
+systemctl enable user &>> $LOGS_FILE
+VALIDATE $? "Enabling user service"
+
+systemctl start user &>> $LOGS_FILE
+VALIDATE $? "Starting user service"
